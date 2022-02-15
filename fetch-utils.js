cd5c8a9aa6e15e6cebf2413cdd68fc909cbd5b15
aidanliddiard <aidanliddiard@gmail.com>
2022-02-15 10:51:18 -0800

    fetchPosts is working

--- fetch-utils.js
+++ fetch-utils.js
@@ -3,3 +3,7 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZ
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+export async function fetchPosts() {
+    const resp = await client.from('postcards').select('*');
+    console.log(resp);
+}
